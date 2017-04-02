e705451b203678f4d673ac4e6f75a64bda760771
Markus Osterhoff <mo@mo-online.org>
2017-04-02 08:34:08 +0200

    moved config to cpulat.conf

--- cpulat.c
+++ cpulat.c
@@ -10,13 +10,23 @@
 #include <string.h>
 
 int back = 0;
-const char* pidfile = "/var/run/cpulat.pid";
+const char* pidfile  = "/var/run/cpulat.pid";
+const char* confname = "/etc/cpulat.conf";
+
+struct cpulat {
+	char* acname;
+	char* latname;
+	char* tmpname;
+	int sleeptime;
+	int latency;
+};
+struct cpulat conf;
+int config(const char* confname);
 
 int main(int argc, const char* argv[])
 {
 	int ret = 0;
 	pid_t pid;
-	int lat = 250;
 
 	if (argc != 1 && argc != 2)
 	{
@@ -45,28 +55,34 @@ int main(int argc, const char* argv[])
 		if (pid==0) back=1;
 	}
 
+	conf.latname = strdup("/dev/cpu_dma_latency");
+	conf.sleeptime = 5;
+	conf.latency   = 250;
+
 	while (1)
 	{
+		while (config(confname)!=0) sleep(1);
+
 		/* check if we are running on AC */
-		const char* acname  = "/sys/class/power_supply/AC/online";
-		const char* latname = "/dev/cpu_dma_latency";
-		const char* tmpname = "/sys/class/hwmon/hwmon1/temp1_input";
 		int ac, tmp;
 		FILE* f = NULL;
 		int fd  = -1;
 
-		f = fopen(acname, "r");
-		if (!f || fscanf(f, "%d", &ac)!=1 || ac!=1) goto wait;
+		if (conf.acname && strlen(conf.acname))
+		{
+			f = fopen(conf.acname, "r");
+			if (!f || fscanf(f, "%d", &ac)!=1 || ac!=1) goto wait;
+		}
 
-		fd = open(latname, O_WRONLY);
+		fd = open(conf.latname, O_WRONLY);
 		if (fd < 0) goto wait;
-		if (write(fd, &lat, sizeof(lat)) != sizeof(lat)) goto wait;
+		if (write(fd, &conf.latency, sizeof(conf.latency)) != sizeof(conf.latency)) goto wait;
 
 wait:
-		sleep(5);
+		sleep(conf.sleeptime);
 		if (f)    fclose(f); f  = NULL;
 
-		f = fopen(tmpname, "r");
+		f = fopen(conf.tmpname, "r");
 		if (f && fscanf(f, "%d", &tmp)==1 && back==0)
 		{
 			fprintf(stdout, "\r%.1f℃", 0.001*tmp);
@@ -81,3 +97,33 @@ cleanup:
 	return ret;
 }
 
+int config(const char* confname)
+{
+	int ret = 0;
+	FILE* f = fopen(confname, "r");
+	if (f == NULL) goto cleanup;
+
+	if (conf.acname)  free(conf.acname);  conf.acname  = NULL;
+	if (conf.tmpname) free(conf.tmpname); conf.tmpname = NULL;
+
+	while (!feof(f))
+	{
+		char line[256];
+		char buf[256];
+		int value;
+		memset(line, 0, sizeof(line));
+		memset(buf, 0, sizeof(buf));
+		if (fgets(line, sizeof(line)-1, f) == NULL) continue;
+		if (line[0] == '#') continue;
+
+		if (sscanf(line, "LATENCY_FILE %64s\n",     buf)==1) { free(conf.latname); conf.latname = strdup(buf); }
+		if (sscanf(line, "TEMPERATURE_FILE %64s\n", buf)==1) { free(conf.tmpname); conf.tmpname = strdup(buf); }
+		if (sscanf(line, "CHECK_AC_FILE %64s\n",    buf)==1) { free(conf.acname ); conf.acname  = strdup(buf); }
+		if (sscanf(line, "LATENCY %d\n",         &value)==1) { conf.latency   = value; }
+		if (sscanf(line, "SLEEP %d\n",           &value)==1) { conf.sleeptime = value; }
+	}
+
+cleanup:
+	if (f) fclose(f); f = NULL;
+	return ret;
+}
